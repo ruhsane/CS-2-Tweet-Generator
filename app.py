96441e7f2c6dae727c3f57102d8e36f4ccc6a6ac
Rushani Shawuti <ruhsane924@gmail.com>
2018-12-13 15:16:12 -0800

    import whole markov file to app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from Markov_Chain2 import Markov
+import Markov_Chain2
 
 app = Flask(__name__)
 
-app.word_list = Markov.words_list("/Users/ruhsane/dev/courses/cs1.2/CS-2-Tweet-Generator/story.txt")
-app.markov = Markov.model(app.word_list)
+app.word_list = Markov_Chain2.Markov.words_list("/Users/ruhsane/dev/courses/cs1.2/CS-2-Tweet-Generator/story.txt")
+app.markov = Markov_Chain2.Markov.model(app.word_list)
 # print(data_structure)
 
 @app.route('/')
 def hello_world():
-    return Markov.result_sentence(app.markov)
+    return Markov_Chain2.Markov.result_sentence(app.markov)
