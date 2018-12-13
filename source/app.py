10985db62bae87a01dd8725d440f940802666002
Rushani Shawuti <ruhsane924@gmail.com>
2018-12-13 03:47:32 -0800

    can run locally with flask

---
+++ source/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from Markov_Chain2 import Markov
+
+app = Flask(__name__)
+
+app.word_list = Markov.words_list("/Users/ruhsane/dev/courses/cs1.2/CS-2-Tweet-Generator/source/story.txt")
+app.markov = Markov.model(app.word_list)
+# print(data_structure)
+
+@app.route('/')
+def hello_world():
+    return Markov.result_sentence(app.markov)
