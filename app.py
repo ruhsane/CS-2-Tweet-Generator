d6a66cc1e7c282d184b57290e3c49b263f047395
Rushani Shawuti <ruhsane924@gmail.com>
2018-12-13 16:52:33 -0800

    clean up files

--- app.py
+++ app.py
@@ -9,3 +9,4 @@ app.markov = Markov_Chain2.Markov.model(app.word_list)
 @app.route('/')
 def hello_world():
     return Markov_Chain2.Markov.result_sentence(app.markov)
+    return  render_template("home.html", sentence= markov_model.random_walk(15))
