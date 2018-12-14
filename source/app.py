74cc815da9e1dc065a2f0d13e8df6506618e6df7
Rushani Shawuti <ruhsane924@gmail.com>
2018-12-13 16:12:58 -0800

    .

--- source/app.py
+++ source/app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 app.word_list = Markov_Chain2.Markov.words_list("story.txt")
 app.markov = Markov_Chain2.Markov.model(app.word_list)
-# print(data_structure)
 
 @app.route('/')
 def hello_world():
