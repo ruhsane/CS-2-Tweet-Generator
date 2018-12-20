2203fd96859be8f61e0bd3260c6e0a4f57818717
Rushani Shawuti <ruhsane924@gmail.com>
2018-12-20 00:47:18 -0500

    added some css (trump picture background with sentence over the img)

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from flask import render_template
 import Markov_Chain2
 
 app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    TEMPLATES_AUTO_RELOAD=True
+)
 
 app.word_list = Markov_Chain2.Markov.words_list("story.txt")
 app.markov = Markov_Chain2.Markov.model(app.word_list)
 
 @app.route('/')
+
+
 def hello_world():
     # return
     string = Markov_Chain2.Markov.result_sentence(app.markov).decode('utf-8')
