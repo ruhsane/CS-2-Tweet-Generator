17bbf566567d9e4f95a83a19f8eed819ab91bbb0
Rushani Shawuti <ruhsane924@gmail.com>
2019-01-30 22:40:44 -0800

    solved server error. updated to python3

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import sys
 from flask import Flask
 from flask import render_template
 
 import Markov_Chain2
+
+print('Python version: ' + sys.version)
 
 app = Flask(__name__)
 app.config.update(
@@ -13,9 +16,10 @@ app.word_list = Markov_Chain2.Markov.words_list("story.txt")
 app.markov = Markov_Chain2.Markov.model(app.word_list)
 
 @app.route('/')
-
-
 def hello_world():
     # return
-    string = Markov_Chain2.Markov.result_sentence(app.markov).decode('utf-8')
+    string = Markov_Chain2.Markov.result_sentence(app.markov)
     return render_template("home.html", sentence = string)
+
+if __name__ == '__main__':
+    hello_world()
